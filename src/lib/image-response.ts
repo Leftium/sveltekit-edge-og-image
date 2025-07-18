ee81877e4ea7760ab548fd3d704cce024a8bf364
Leftium <381217+Leftium@users.noreply.github.com>
2025-07-18 16:29:40 -0500

    Use Wrangler’s wasm_modules binding.

--- src/lib/image-response.ts
+++ src/lib/image-response.ts
@@ -9,119 +9,112 @@ import { Resvg, initWasm } from '@resvg/resvg-wasm';
 //import wasmUrl from '@resvg/resvg-wasm/index_bg.wasm?url';
 //import wasmModule from '@resvg/resvg-wasm/index_bg.wasm?module';
 
-// @ts-expect-error import wasm file normally
-import wasmModule from '@resvg/resvg-wasm/index_bg.wasm';
+// import wasmModule from '@resvg/resvg-wasm/index_bg.wasm';
 
 //import wasmModule from "$lib/index_bg.wasm";
 
 //import fs from 'node:fs/promises';
 
-
 export interface ImageResponseOptions {
-    width?: number;
-    height?: number;
-    fonts?: SatoriOptions['fonts'];
-    debug?: boolean;
-    text?: string;
-    spanText?: string;
-    status?: number;
-    statusText?: string;
-    headers?: Record<string, string>;
-    tailwindConfig?: SatoriOptions['tailwindConfig'];
+	width?: number;
+	height?: number;
+	fonts?: SatoriOptions['fonts'];
+	debug?: boolean;
+	text?: string;
+	spanText?: string;
+	status?: number;
+	statusText?: string;
+	headers?: Record<string, string>;
+	tailwindConfig?: SatoriOptions['tailwindConfig'];
 }
 
 let wasmInitialized = false;
 
 export const generateImage = async <T extends Record<string, unknown>>(
-    element: Component<T>,
-    options: ImageResponseOptions,
+	element: Component<T>,
+	options: ImageResponseOptions
 ) => {
+	const { fetch } = getRequestEvent();
 
-    const { fetch } = getRequestEvent();
+	if (!wasmInitialized) {
+		try {
+			//const wasmPath = process.cwd() + wasmUrl;
+			//const wasmBuffer = await fs.readFile(wasmPath);
 
+			//const response = await fetch(wasmUrl);
+			//const wasmBuffer = new Uint8Array(await response.arrayBuffer());
+			//await initWasm(wasmInline);
 
-    if (!wasmInitialized) {
-        try {
-            //const wasmPath = process.cwd() + wasmUrl;
-            //const wasmBuffer = await fs.readFile(wasmPath);
+			//const wasmModule = await import(/* @vite-ignore */ wasmUrl);
 
-            //const response = await fetch(wasmUrl);
-            //const wasmBuffer = new Uint8Array(await response.arrayBuffer());
-            //await initWasm(wasmInline);
+			//console.log(wasmModule);
 
-            //const wasmModule = await import(/* @vite-ignore */ wasmUrl);
+			// `WASM.resvg` is a pre-compiled WebAssembly.Module
+			await initWasm(WASM.resvg);
 
-            //console.log(wasmModule);    
+			wasmInitialized = true;
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-            await initWasm(wasmModule);
+	const { text, spanText } = options;
 
-            wasmInitialized = true;
-        } catch (e) {
-            console.error(e);
-        }
-    }
+	const renderedSvelte = render(element as Component, { props: { text, spanText } });
 
+	const fontFile = await fetch('https://og-playground.vercel.app/inter-latin-ext-700-normal.woff');
+	const fontData: ArrayBuffer = await fontFile.arrayBuffer();
 
-    const { text, spanText } = options;
+	options.fonts = [
+		{
+			name: 'Inter Latin',
+			data: fontData,
+			style: 'normal'
+		}
+	];
 
-    const renderedSvelte = render(element as Component, { props: { text, spanText } });
+	const elementHtml = toReactElement(renderedSvelte.body);
 
-    const fontFile = await fetch(
-        'https://og-playground.vercel.app/inter-latin-ext-700-normal.woff',
-    );
-    const fontData: ArrayBuffer = await fontFile.arrayBuffer();
+	const svg = await satori(elementHtml, {
+		width: options.width || 1200,
+		height: options.height || 630,
+		fonts: options.fonts?.length ? options.fonts : [],
+		tailwindConfig: options.tailwindConfig
+	});
 
-    options.fonts = [
-        {
-            name: 'Inter Latin',
-            data: fontData,
-            style: 'normal',
-        },
-    ];
+	const svgBuffer = Buffer.from(svg);
 
-    const elementHtml = toReactElement(renderedSvelte.body);
+	const png = new Resvg(svgBuffer, {
+		fitTo: {
+			mode: 'width',
+			value: options.width || 1200
+		}
+	});
 
-    const svg = await satori(elementHtml, {
-        width: options.width || 1200,
-        height: options.height || 630,
-        fonts: options.fonts?.length ? options.fonts : [],
-        tailwindConfig: options.tailwindConfig,
-    });
+	const pngBuffer = png.render().asPng();
 
-    const svgBuffer = Buffer.from(svg);
-
-    const png = new Resvg(svgBuffer, {
-        fitTo: {
-            mode: 'width',
-            value: options.width || 1200
-        }
-    });
-
-    const pngBuffer = png.render().asPng();
-
-    return pngBuffer;
+	return pngBuffer;
 };
 
 export class ImageResponse<T extends Record<string, unknown>> extends Response {
+	constructor(element: Component<T>, options: ImageResponseOptions = {}) {
+		super();
 
-    constructor(element: Component<T>, options: ImageResponseOptions = {}) {
-        super();
+		const body = new ReadableStream({
+			async start(controller) {
+				const buffer = await generateImage(element, options);
+				controller.enqueue(buffer);
+				controller.close();
+			}
+		});
 
-        const body = new ReadableStream({
-            async start(controller) {
-                const buffer = await generateImage(element, options);
-                controller.enqueue(buffer);
-                controller.close();
-            },
-        });
-
-        return new Response(body, {
-            headers: {
-                'content-type': 'image/png',
-                'cache-control': 'public, immutable, no-transform, max-age=31536000'
-            },
-            status: options.status || 200,
-            statusText: options.statusText,
-        });
-    }
+		return new Response(body, {
+			headers: {
+				'content-type': 'image/png',
+				'cache-control': 'public, immutable, no-transform, max-age=31536000'
+			},
+			status: options.status || 200,
+			statusText: options.statusText
+		});
+	}
 }
